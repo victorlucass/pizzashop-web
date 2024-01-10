3185c2d52661563935d37f93bcbd04450912431c
Victor Lucas <victorlucas@MacBook-Air-de-Victor.local>
2024-01-09 22:36:54 -0400

    feat: configs eslint e prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import '@/global.css'
+
+import { Button } from './components/ui/button'
 export function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  );
+    <div className="flex h-screen items-center justify-center">
+      <Button>enviar</Button>
+    </div>
+  )
 }
