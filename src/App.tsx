7fa01f5761de0be0926074f9d3eff4ba06cd66d3
Victor Lucas <victorlucas@MacBook-Air-de-Victor.local>
2024-01-18 00:16:20 -0400

    feat: create toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { routes } from './routes'
 export function App() {
   return (
-    <HelmetProvider>
-      <Toaster richColors />
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={routes} />
-    </HelmetProvider>
+    <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+      <HelmetProvider>
+        <Toaster richColors />
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <RouterProvider router={routes} />
+      </HelmetProvider>
+    </ThemeProvider>
   )
 }
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 
 import { NavLink } from './nav-link'
+import { ThemeToggle } from './theme/theme-toggle'
 import { Separator } from './ui/separator'
 
 export function HeaderApp() {
@@ -19,6 +20,10 @@ export function HeaderApp() {
             Pedidos
           </NavLink>
         </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+        </div>
       </div>
     </div>
   )
