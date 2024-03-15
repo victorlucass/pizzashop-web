992e5b5fd4a548657ae7089ebe1071697d751873
Victor Lucas <victorlucas@MacBook-Air-de-Victor.local>
2024-03-15 09:15:59 -0400

    feat: create revenue chart

---
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,81 @@
+import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const data = [
+  {
+    date: '10/12',
+    revenue: 1248.6,
+  },
+  {
+    date: '11/12',
+    revenue: 600.6,
+  },
+  {
+    date: '12/12',
+    revenue: 800.6,
+  },
+  {
+    date: '13/12',
+    revenue: 300.6,
+  },
+  {
+    date: '14/12',
+    revenue: 2248.6,
+  },
+  {
+    date: '15/12',
+    revenue: 992.6,
+  },
+  {
+    date: '16/12',
+    revenue: 3000.6,
+  },
+]
+
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no período
+          </CardTitle>
+          <CardDescription>Receita diária no período</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(v: number) =>
+                v.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })
+              }
+            />
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="revenue"
+              stroke={colors.violet['500']}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
